4938ba04d3f99e74f2e6aff7196f4be5366012a6
kant-sdev <kaua.cantanhede.santos@gmail.com>
2025-08-05 18:04:54 -0300

    "fix: corrige alinhamento da landing page versão desktop"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,9 @@ import LandingPage from '@/components/pages/landing-page'
 import React from 'react'
 
 export default function Home() {
-  return <LandingPage/>
+  return (
+    <div className='w-full'>
+      <LandingPage/>
+    </div>
+  )
 }
